b4917ccdb64c2b9e540dc7b2dff32d6a697f58ee
xiedong <dxiesjz@163.com>
2024-07-08 23:01:47 +0800

    rename

--- demos-java/java-middle-demo/hystrx-outer/src/main/java/xxd/demos/hystrx/outer/TestServerApp.java
+++ demos-java/java-middle-demo/hystrx-outer/src/main/java/xxd/demos/hystrx/outer/TestServerApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HystrxOuterApplication {
+public class TestServerApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(HystrxOuterApplication.class, args);
+        SpringApplication.run(TestServerApp.class, args);
     }
 
 }
--- demos-java/java-middle-demo/hystrx-outer/src/main/java/xxd/demos/hystrx/outer/controller/TestServerController.java
+++ demos-java/java-middle-demo/hystrx-outer/src/main/java/xxd/demos/hystrx/outer/controller/TestServerController.java
@@ -1,6 +1,5 @@
 package xxd.demos.hystrx.outer.controller;
 
-import cn.hutool.core.util.RandomUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,13 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/outer/user/api")
 @Slf4j
-public class UserController {
+public class TestServerController {
 
     @GetMapping("/nickname/query")
     public String getNickname(@RequestParam long userId) throws InterruptedException {
         log.info("getNickname-start,userId:{}", userId);
         // http调用
-        Thread.sleep(RandomUtil.randomInt(50));
+        Thread.sleep(200);
         return "测试用户名：" + userId;
     }
 }
